11ac741c1adc701037030d1a320a7f477e42e9c8
asabriye <abdullahisabriye@gmail.com>
2021-11-09 14:19:51 +0000

    updated to fetch co-ordinates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const  {fetchMyIP, fetchCoordsByIP, fetchIssFlyOverTimes}  = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -9,3 +9,22 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
 }); 
+fetchCoordsByIP('142.214.83.107', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+const exampleCoords = { latitude: '43.7137', longitude: '-79.5964' };
+
+fetchIssFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
